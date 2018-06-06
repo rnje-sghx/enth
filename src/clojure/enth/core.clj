c66f2451d10e10f738289ec57b9c8aa1082a8996
Ranjeet Singh <ranjeet@raeoks.com>
2018-06-06 20:46:03 +0530

    Refactor nth to composition

--- src/clojure/enth/core.clj
+++ src/clojure/enth/core.clj
@@ -1,9 +1,16 @@
 (ns enth.core)
 
+(defn- make-nth-fn
+  ([index]
+   (make-nth-fn index identity))
+  ([index nth-fn]
+   (if (<= index 0)
+     (comp first nth-fn)
+     (recur (- index 1) (comp rest nth-fn)))))
+
 (defn nth
   "Returns the value at the index. get returns nil if index out of bounds.
    nth also works for Strings and Lists, and, in O(n) time, for sequences."
   [collection index]
-  (if (<= index 0)
-    (first collection)
-    (recur (rest collection) (- index 1))))
+  (let [nth-fn (make-nth-fn index)]
+    (nth-fn collection)))
